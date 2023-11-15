fc77604300adb9292de89f0fb7005562d347639b
Cooper Hanson <cooperhanson2020@gmail.com>
2023-11-14 21:45:48 -0600

    Got rid of the ignorables

--- KnO3/target/debug/KnO3.d
+++
@@ -1,1 +0,0 @@
-C:\GitHub\KnO3\KnO3\target\debug\KnO3.exe: C:\GitHub\KnO3\KnO3\src\main.rs
--- KnO3/target/debug/deps/KnO3-216b830dfc058585.d
+++
@@ -1,5 +0,0 @@
-C:\GitHub\KnO3\KnO3\target\debug\deps\KnO3-216b830dfc058585.rmeta: src\main.rs
-
-C:\GitHub\KnO3\KnO3\target\debug\deps\KnO3-216b830dfc058585.d: src\main.rs
-
-src\main.rs:

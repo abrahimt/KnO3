c3d41a9d2bf5544c5515d34623e2a1cb0045f5a5
abrahimt <abrahimtoutoungi@gmail.com>
2023-11-15 17:23:06 -0600

    Update main.rs
    Updated based on code review.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
 //min function
 pub fn min<T: Ord>(a: T, b: T) -> T {
-    if a < b {
-        a
-    } else {
-        b
-    }
+    if a < b { a } else { b }
 }
 //max function
 pub fn max<T: Ord>(a: T, b: T) -> T {
-    if a > b {
-        a
-    } else {
-        b
-    }
+    if a > b { a } else { b }
 }
 
 fn main() {

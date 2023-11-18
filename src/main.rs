229611e459992da3229c45b0203264e3a0daa9fb
Cooper Hanson <cooperhanson2020@gmail.com>
2023-11-17 19:26:07 -0600

    Removed unused imports from testing

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,8 @@
 mod board;
 use board::Chessboard;
-use std::io::stdout;
-use crossterm::{
-    execute,
-    style::{ Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-};
 
 fn main() {
     let cb = Chessboard::new();
-    /*
-    execute!(
-        stdout(),
-        SetForegroundColor(Color::Blue),
-        SetBackgroundColor(Color::Red),
-        Print("Styled text here."),
-        ResetColor
-        );
-        */
     cb.print(true);
 }
 

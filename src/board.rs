67bdf32d145fc2cda344de45af107901d43c1357
Cooper Hanson <cooperhanson2020@gmail.com>
2023-11-17 19:40:48 -0600

    Revert "Merge branch '11-create-fen-function-under-board-struct' into 27-termion-cant-build-on-windows"
    This reverts commit d120d09bda1b52f92a64ea4b7358811d04c378eb, reversing
    changes made to 7629be47e9619d36d113189392b040bc7933995f.

--- src/board.rs
+++ src/board.rs
@@ -5,6 +5,7 @@ use crossterm::{
 };
 
 
+
 /// Struct representing a chessboard with piece positions and game state
 /// Each `piece` is a uint64 bitmap. Each byte represents a rank and a 1 indicates a presence in
 /// that position.
@@ -28,7 +29,6 @@ pub struct Chessboard {
 }
 
 impl Chessboard {
-  
     /// Create a new instance of a chessboard, setup to start a new game.
     pub fn new() -> Chessboard {
         Chessboard {
@@ -44,11 +44,13 @@ impl Chessboard {
             black_king: 0b0000100000000000000000000000000000000000000000000000000000000000,
             black_queen: 0b0001000000000000000000000000000000000000000000000000000000000000,
             black_rooks: 0b1000000100000000000000000000000000000000000000000000000000000000,
-            castling_rights: 0b1111,
+            white_castle: 3,
+            black_castle: 3,
             en_passant: 0,
-            white_turn: true,
+            white_turn: true
         }
     }
+
 
     /// Prints the chessboard to the console
     /// * `pretty` - Print with extra formatting
@@ -60,10 +62,7 @@ impl Chessboard {
             print!("{rank} ");
             for file in 0..files.len() {
                 let piece = self.piece_at_position(*rank, file);
-                if !pretty {
-                    print!("{piece} ");
-                    continue;
-                }
+                if !pretty { print!("{piece} "); continue; }
 
                 let fg = self.find_fg(piece);
                 let frmt_piece = format!("{:^3}", piece);
@@ -106,7 +105,7 @@ impl Chessboard {
             ('b', self.black_bishops),
             ('k', self.black_king),
             ('q', self.black_queen),
-            ('r', self.black_rooks),
+            ('r', self.black_rooks)
         ]
     }
 
@@ -132,275 +131,97 @@ impl Chessboard {
     /// # Return:
     /// The character representation of the piece at this position.
     /// If there is no piece here it will return a period.
-    fn piece_at_position(&self, rank: usize, file: usize) -> char {
+    fn piece_at_position(&self, rank: usize, file: usize) -> char { 
         for (p_type, positions) in self.get_pieces() {
             let rank_byte = positions >> ((rank - 1) * 8);
-            if (rank_byte & (1 << file)) != 0 {
-                return p_type;
-            }
+            if (rank_byte & (1 << file)) != 0 { return p_type; }
         }
         '.'
     }
 
+
     fn whose_turn(&self) -> &str {
-        if self.white_turn {
-            "white"
-        } else {
-            "black"
-        }
+        if self.white_turn { "white" } else { "black" }
     }
 
-    // Parser function that converts a FEN (Forsyth–Edwards Notation) string to a Chessboard struct
-    pub fn from_string(&self, fen: &str) -> Chessboard {
-        // Initialize a new Chessboard with default values
-        let mut chessboard = Chessboard {
-            // Initialize bitboards for each piece and other game state variables
-            black_pawns: 0,
-            black_rooks: 0,
-            black_knights: 0,
-            black_bishops: 0,
-            black_queen: 0,
-            black_king: 0,
-            white_pawns: 0,
-            white_rooks: 0,
-            white_knights: 0,
-            white_bishops: 0,
-            white_queen: 0,
-            white_king: 0,
-            castling_rights: 0,
-            white_turn: true,
-            en_passant: 0,
-        };
 
-        // Split the FEN string into parts using ' ' as the delimiter
-        let fen_parts: Vec<&str> = fen.split_whitespace().collect();
-
-        // Parse the piece placement part of the FEN string
-        let board_rows: Vec<&str> = fen_parts[0].split('/').collect();
-        for (mut rank, row) in board_rows.iter().rev().enumerate() {
-            rank += 1;
-            let mut file = 0; // Initialize the file (column) index
-            for piece in row.chars() {
-                if piece.is_digit(10) {
-                    let empty_squares = piece.to_digit(10).unwrap() as usize;
-                    file += empty_squares; // Skip empty squares
-                } else {
-                    let square_index = 8 * (rank - 1) + file;
-                    // Update the corresponding bitboard based on the piece type and color
-                    if piece.is_ascii_lowercase() {
-                        // Black pieces
-                        match piece {
-                            'p' => chessboard.black_pawns |= pow(2, square_index),
-                            'r' => chessboard.black_rooks |= pow(2, square_index),
-                            'b' => chessboard.black_bishops |= pow(2, square_index),
-                            'k' => chessboard.black_king |= pow(2, square_index),
-                            'q' => chessboard.black_queen |= pow(2, square_index),
-                            'n' => chessboard.black_knights |= pow(2, square_index),
-                            _ => { /* Handle other lowercase characters if needed */ }
-                        }
-                    } else {
-                        // White pieces
-                        match piece {
-                            'P' => chessboard.white_pawns |= pow(2, square_index),
-                            'R' => chessboard.white_rooks |= pow(2, square_index),
-                            'B' => chessboard.white_bishops |= pow(2, square_index),
-                            'K' => chessboard.white_king |= pow(2, square_index),
-                            'Q' => chessboard.white_queen |= pow(2, square_index),
-                            'N' => chessboard.white_knights |= pow(2, square_index),
-                            _ => { /* Handle other uppercase characters if needed */ }
-                        }
-                    }
-                    file += 1; // Move to the next file
-                }
-            }
-        }
-
-        // Parse whose turn it is
-        chessboard.white_turn = fen_parts[1] == "w";
-
-        // Parse castling rights
-        let fen_castle = fen_parts[2];
-        let mut castles = 0;
-        for c in fen_castle.chars() {
-            let v = match c {
-                'K' => 0b1000,
-                'Q' => 0b0100,
-                'k' => 0b0010,
-                'q' => 0b0001,
-                _ => 0b0,
-            };
-            castles |= v;
-        }
-        chessboard.castling_rights = castles;
-
-        // Parse en passant square
-        let fen_passant = fen_parts[3];
-        if fen_passant != "-" {
-            if let (Some(col), Some(row)) = (
-                fen_passant.chars().nth(0).map(|c| c.to_ascii_uppercase()),
-                fen_passant.chars().nth(1).and_then(|c| c.to_digit(10)),
-            ) {
-                if (1..=8).contains(&row) {
-                    let col_value = match col {
-                        'A' => 1,
-                        'B' => 2,
-                        'C' => 3,
-                        'D' => 4,
-                        'E' => 5,
-                        'F' => 6,
-                        'G' => 7,
-                        'H' => 8,
-                        _ => 0, // Handle unexpected characters
-                    };
-                    chessboard.en_passant = col_value + 8 * (row - 1);
-                }
-            }
-        }
-
-        // Ignore the rest of the FEN string for now
-        return chessboard;
-    }
-
-    // Serializer function that converts a Chessboard struct to a FEN (Forsyth–Edwards Notation) string
-    pub fn to_string(chessboard: Chessboard) -> String {
-        // Initialize a vector to store FEN components as strings
-        let mut string_array: Vec<String> = Vec::with_capacity(6);
-        let fen_string;
-
-        // Piece placement
-        for rank in (1..=8).rev() {
-            let mut empty_squares = 0;
-            let mut row_string = String::new();
-
-            // Iterate through each file (column) in the rank
-            for file in 1..=8 {
-                let square_index = (rank - 1) * 8 + (file - 1);
-
-                // Determine the piece on the current square
-                let piece = if (chessboard.white_pawns >> square_index) & 1 != 0 {
-                    'P'
-                } else if (chessboard.white_rooks >> square_index) & 1 != 0 {
-                    'R'
-                } else if (chessboard.white_knights >> square_index) & 1 != 0 {
-                    'N'
-                } else if (chessboard.white_bishops >> square_index) & 1 != 0 {
-                    'B'
-                } else if (chessboard.white_queen >> square_index) & 1 != 0 {
-                    'Q'
-                } else if (chessboard.white_king >> square_index) & 1 != 0 {
-                    'K'
-                } else if (chessboard.black_pawns >> square_index) & 1 != 0 {
-                    'p'
-                } else if (chessboard.black_rooks >> square_index) & 1 != 0 {
-                    'r'
-                } else if (chessboard.black_knights >> square_index) & 1 != 0 {
-                    'n'
-                } else if (chessboard.black_bishops >> square_index) & 1 != 0 {
-                    'b'
-                } else if (chessboard.black_queen >> square_index) & 1 != 0 {
-                    'q'
-                } else if (chessboard.black_king >> square_index) & 1 != 0 {
-                    'k'
-                } else {
-                    empty_squares += 1;
-                    continue;
-                };
-
-                // Handle empty squares and append piece to the row string
-                if empty_squares > 0 {
-                    row_string.push_str(&empty_squares.to_string());
-                    empty_squares = 0;
-                }
-                row_string.push(piece);
-            }
-
-            // Append the count of empty squares at the end of the row string
-            if empty_squares > 0 {
-                row_string.push_str(&empty_squares.to_string());
-            }
-
-            // Add the row string to the FEN components vector
-            string_array.push(row_string);
-        }
-
-        // Whose turn
-        string_array.push(if chessboard.white_turn {
-            "w ".to_string()
-        } else {
-            "b ".to_string()
-        });
-
-        // Castling rights
-        string_array.push(match chessboard.castling_rights {
-            0 => "- ".to_string(),
-            rights => {
-                let mut rights_string = String::new();
-
-                // Check individual castling rights and append to rights_string
-                if rights & 0b1000 != 0 {
-                    rights_string.push('K');
-                }
-                if rights & 0b0100 != 0 {
-                    rights_string.push('Q');
-                }
-                if rights & 0b0010 != 0 {
-                    rights_string.push('k');
-                }
-                if rights & 0b0001 != 0 {
-                    rights_string.push('q');
-                }
-
-                rights_string
-            }
-        });
-
-        // En passant
-        if chessboard.en_passant == 0 {
-            string_array.push("- ".to_string());
-        } else {
-            // Convert en passant square to algebraic notation
-            let row = (chessboard.en_passant - 1) / 8 + 1;
-            let col = (chessboard.en_passant - 1) % 8;
-
-            let column_char = (b'A' + col) as char;
-
-            string_array.push(format!("{}{}", column_char, row));
-        }
-
-        // Set the rest to default values
-        string_array.push("0 ".to_string());
-        string_array.push("1".to_string());
-
-        // Combine array elements into a single string
-        fen_string = string_array.concat();
-
-        // Print the FEN string (for debugging purposes)
-        println!("{:?}", fen_string);
-
-        // Return the FEN string
-        fen_string
-    }
+    //MINIMAX Function Pseudo-code
+    // fn minimax(position, depth, alpha, beta, maximixing_player) {
+    //     if depth == 0 or game over in position
+    //         return static evaluation of position
+    //     if maximizing_player (white)
+    //         max_eval = -infinity
+    //         for each child of position
+    //             eval = minimax(child, depth - 1, alpha, beta, false)
+    //             max_eval = max(max_eval, eval)
+    //             alpha = max(alpha, eval)
+    //             if beta <= alpha
+    //                 break
+    //         return max_eval
+    //     else
+    //         min_eval = +infinity
+    //         for each child of position
+    //         eval = minimax(child, depth - 1, alpha, beta, true)
+    //         min_eval = min(min_eval, eval)
+    //         beta = min(beta, eval)
+    //         if beta <= alpha
+    //             break
+    //     return min_eval
+    // }
 }
-//MINIMAX Function Pseudo-code
-// fn minimax(position, depth, alpha, beta, maximixing_player) {
-//     if depth == 0 or game over in position
-//         return static evaluation of position
-//     if maximizing_player (white)
-//         max_eval = -infinity
-//         for each child of position
-//             eval = minimax(child, depth - 1, alpha, beta, false)
-//             max_eval = max(max_eval, eval)
-//             alpha = max(alpha, eval)
-//             if beta <= alpha
-//                 break
-//         return max_eval
-//     else
-//         min_eval = +infinity
-//         for each child of position
-//         eval = minimax(child, depth - 1, alpha, beta, true)
-//         min_eval = min(min_eval, eval)
-//         beta = min(beta, eval)
-//         if beta <= alpha
-//             break
-//     return min_eval
+//Below here is just ideas for functions:
+
+//fn what_occupy square() {
+//     check each piece type with the square and see which returns true
+// }
+
+// fn is_square_occupied(square) {
+// bitwise and every piece together
+// search through the new u64 and see where there are 1s
+// return the indices of the 1s
+// }
+
+// en_passant_square() {
+//     // Return the square where an en passant capture is possible, or -1 if not possible
+//     // For example, if the last move was a double-step pawn move, return the square where the opponent's pawn can capture en passant.
+// }
+
+// castling_rights() {
+//     // Use flags or bitmasks to represent castling rights for both players
+//     // For example, if kingside castling is allowed for white, set white_kingside_castle = true
+//     // Similarly, maintain flags for black's castling rights
+// }
+
+// make_move(move) {
+//     // Update the bitboards and game state based on the move
+//     // For example, if it's a pawn move, update the pawn bitboard
+//     // If it's a capture, clear the bit for the captured piece
+//     // Update en passant square and castling rights accordingly
+//     // Switch turns: white_turn = !white_turn;
+// }
+
+// generate_legal_moves() {
+//     legal_moves = []
+
+//     for each piece in pieces_of_current_turn {
+//         possible_moves = generate_moves_for_piece(piece)
+//         for each move in possible_moves {
+//             if is_legal_move(move) {
+//                 legal_moves.push(move)
+//             }
+//         }
+//     }
+//
+//     return legal_moves
+// }
+
+// generate_moves_for_piece(piece) {
+//     // Generate all possible moves for the given piece
+//     // Consider piece-specific movement rules (e.g., pawn's initial double move, castling for king, etc.)
+// }
+
+// is_legal_move(move) {
+//     // Check if the given move is legal
+//     // Verify that the move adheres to the rules of chesce-s, including piespecific rules and board state
+//     // Check for legality includes considerations like not moving into check, en passant captures, castling rules, etc.
 // }

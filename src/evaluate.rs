951d01a294564a0b0875c282632386718997d562
Abe <abrahimtoutoungi@gmail.com>
2023-11-20 02:21:40 -0700

    Quick read through
    Changed a few things we won't need

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -1,29 +1,20 @@
-// // Pseudo-code for evaluating positions
-
-// // Define a struct or type representing the game state
-// GameState {
-//     // Fields representing the state of the game (e.g., board, turn, pieces)
-// }
-
-// // Pseudo-code for evaluating the current game state
 // function evaluate_position(game_state):
-//     // Extract relevant information from the game state
+// Extract relevant information from the game state
 //     board = game_state.get_board()
 //     player_turn = game_state.get_player_turn()
 
-//     // Evaluate the position based on different factors
+// Evaluate the position based on different factors
 //     material_score = evaluate_material(board)
 //     mobility_score = evaluate_mobility(board, player_turn)
 //     positional_score = evaluate_positional(board, player_turn)
 
-//     // Combine individual scores to get an overall position evaluation
+// Combine individual scores to get an overall position evaluation
 //     overall_score = combine_scores(material_score, mobility_score, positional_score)
-
 //     return overall_score
 
-// // Pseudo-code for evaluating material on the board
+
 // function evaluate_material(board):
-//     // Sum up the values of pieces for both players
+       // Sum up the values of pieces for both players
 //     white_material = sum_piece_values(board, White)
 //     black_material = sum_piece_values(board, Black)
 
@@ -69,4 +60,4 @@
 
 //     return combined_score
 
-// // Other helper functions for counting legal moves, calculating positional scores, etc.
+// Other helper functions for counting legal moves, calculating positional scores, etc.

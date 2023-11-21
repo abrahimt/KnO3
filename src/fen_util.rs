249d2a22cc69d92f8e4ea620b91887aa0e7e0fa0
Cooper Hanson <cooperhanson2020@gmail.com>
2023-11-21 17:52:19 -0600

    Added a fake unit test in fen_util

--- src/fen_util.rs
+++ src/fen_util.rs
@@ -207,3 +207,14 @@ pub fn parse_en_passant(chessboard: &mut Chessboard, en_passant: &str) {
         }
     }
 }
+
+/* ***** */
+/* TESTS */
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}

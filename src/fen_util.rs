d46d707ace3be42d00154e1d11a1310aec8d5dae
Abe <abrahimtoutoungi@gmail.com>
2023-11-22 15:44:28 -0700

    Changes according to PR review

--- src/fen_util.rs
+++ src/fen_util.rs
@@ -139,20 +139,6 @@ pub fn get_fen_placement(chessboard: &Chessboard) -> String {
 /// # Returns
 /// 
 /// A string representing the castling rights in FEN format ("- " if no castling rights).
-/// 
-/// # Example
-/// 
-/// ```
-/// use kn_o3::Chessboard;
-/// 
-/// let mut chessboard = Chessboard::new();
-/// chessboard.castling_rights = 0b1111; // All castling rights available
-/// let castling_rights = get_fen_castles(&chessboard);
-/// assert_eq!(castling_rights, "KQkq");
-/// ```
-/// 
-/// The function retrieves the castling rights from the chessboard and returns them in FEN format.
-/// It filters out the available castling rights for kingside and queenside for both white and black.
 #[rustfmt::skip]
 pub fn get_fen_castles(chessboard: &Chessboard) -> String {
     let state = chessboard.castling_rights;
@@ -180,18 +166,6 @@ pub fn get_fen_castles(chessboard: &Chessboard) -> String {
 ///
 /// A string representing the en passant square in algebraic notation ("-" if no en passant square).
 ///
-/// # Example
-///
-/// ```
-/// use kn_o3::Chessboard;
-///
-/// let mut chessboard = Chessboard::new();
-/// chessboard.en_passant = 29; // Corresponding square for "e3"
-/// let en_passant_square = get_fen_passant(&chessboard);
-/// assert_eq!(en_passant_square, "e3");
-/// ```
-///
-/// The function retrieves the en passant square from the chessboard and returns it in algebraic notation.
 /// If there is no en passant square (0), it returns "-".
 pub fn get_fen_passant(chessboard: &Chessboard) -> String {
     let passant = chessboard.en_passant;
@@ -214,20 +188,6 @@ pub fn get_fen_passant(chessboard: &Chessboard) -> String {
 /// # Returns
 ///
 /// A `Result` indicating success or an error message if the piece placement is invalid.
-///
-/// # Example
-///
-/// ```
-/// use kn_o3::Chessboard;
-///
-/// let mut chessboard = Chessboard::empty();
-/// let piece_placement = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
-/// parse_piece_placement(&mut chessboard, piece_placement).expect("Invalid piece placement");
-/// ```
-///
-/// The function parses the piece placement part of the FEN string and updates the chessboard.
-/// It expects a string with piece positions for each rank, separated by slashes, and updates
-/// the chessboard's piece positions accordingly.
 pub fn parse_piece_placement(
     chessboard: &mut Chessboard,
     piece_placement: &str,
@@ -242,20 +202,6 @@ pub fn parse_piece_placement(
 ///
 /// - `chessboard`: A mutable reference to the `Chessboard` struct to update whose turn it is.
 /// - `whose_turn`: A string representing whose turn it is ("w" for white, "b" for black).
-///
-/// # Example
-///
-/// ```
-/// use kn_o3::Chessboard;
-///
-/// let mut chessboard = Chessboard::empty();
-/// let whose_turn = "w";
-/// parse_whose_turn(&mut chessboard, whose_turn);
-/// assert_eq!(chessboard.white_turn, true);
-/// ```
-///
-/// The function parses the turn information from the FEN string and updates the chessboard
-/// to reflect whose turn it is. It expects "w" for white and "b" for black.
 pub fn parse_whose_turn(chessboard: &mut Chessboard, whose_turn: &str) {
     chessboard.white_turn = whose_turn == "w";
 }
@@ -266,21 +212,6 @@ pub fn parse_whose_turn(chessboard: &mut Chessboard, whose_turn: &str) {
 ///
 /// - `chessboard`: A mutable reference to the `Chessboard` struct to update castling rights.
 /// - `castle_rights`: A string representing the castling rights part of the FEN string.
-///
-/// # Example
-///
-/// ```
-/// use kn_o3::Chessboard;
-///
-/// let mut chessboard = Chessboard::empty();
-/// let castling_rights = "KQkq";
-/// parse_castling_rights(&mut chessboard, castling_rights);
-/// assert_eq!(chessboard.castling_rights, 0b1111);
-/// ```
-///
-/// The function parses the castling rights part of the FEN string and updates the chessboard
-/// to reflect the available castling rights. It expects a string with letters representing
-/// castling rights for kingside and queenside for both white and black.
 pub fn parse_castling_rights(chessboard: &mut Chessboard, castle_rights: &str) {
     for c in castle_rights.chars() {
         let v = match c {
@@ -301,21 +232,6 @@ pub fn parse_castling_rights(chessboard: &mut Chessboard, castle_rights: &str) {
 /// - `chessboard`: A mutable reference to the `Chessboard` struct to update the en passant square.
 /// - `en_passant`: A string representing the en passant square in algebraic notation (e.g., "e3").
 ///   If the en passant square is "-" (no en passant square), it is ignored.
-///
-/// # Example
-///
-/// ```
-/// use kn_o3::Chessboard;
-///
-/// let mut chessboard = Chessboard::new();
-/// parse_en_passant(&mut chessboard, "e3");
-/// assert_eq!(chessboard.en_passant, 29); // Corresponding square for "e3"
-/// ```
-///
-/// The function parses the en passant square from algebraic notation and updates the chessboard's
-/// en passant field. If the input is "-", indicating no en passant square, it is ignored.
-/// The algebraic notation is expected to be in the format "FileRank", where "File" is a letter
-/// from 'A' to 'H', and "Rank" is a digit from 1 to 8.
 pub fn parse_en_passant(chessboard: &mut Chessboard, en_passant: &str) {
     if en_passant != "-" {
         if let (Some(col), Some(row)) = (

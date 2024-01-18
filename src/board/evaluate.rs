61e38301bbae290075a94be002dd9081962d24c7
Cooper Hanson <cooperhanson2020@gmail.com>
2024-01-18 12:59:51 -0600

    implemented very basic scoring

---
+++ src/board/evaluate.rs
@@ -0,0 +1,16 @@
+use super::Chessboard;
+
+impl Chessboard {
+    pub fn get_score(&self) -> i32 {
+        let mut score = 0;
+
+        // Material balance
+        score += 1 * (self.white_pawns.count_ones() as i32 - self.black_pawns.count_ones() as i32);
+        score += 1 * (self.white_knights.count_ones() as i32 - self.black_knights.count_ones() as i32);
+        score += 1 * (self.white_bishops.count_ones() as i32 - self.black_bishops.count_ones() as i32);
+        score += 1 * (self.white_rooks.count_ones() as i32 - self.black_rooks.count_ones() as i32);
+        score += 1 * (self.white_queen.count_ones() as i32 - self.black_queen.count_ones() as i32);
+
+        score
+    }
+}
--- src/board/mod.rs
+++ src/board/mod.rs
@@ -2,6 +2,7 @@ mod chessboard;
 mod displays;
 mod movement;
 mod piece;
+mod evaluate;
 pub mod position;
 
 pub use chessboard::Chessboard;

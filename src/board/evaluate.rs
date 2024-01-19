5005b111ae0fa2ad2434e64161cb8f3ee3c43f02
Cooper Hanson <cooperhanson2020@gmail.com>
2024-01-18 18:06:10 -0600

    Weighted the score of each piece

--- src/board/evaluate.rs
+++ src/board/evaluate.rs
@@ -6,10 +6,10 @@ impl Chessboard {
 
         // Material balance
         score += 1 * (self.white_pawns.count_ones() as i32 - self.black_pawns.count_ones() as i32);
-        score += 1 * (self.white_knights.count_ones() as i32 - self.black_knights.count_ones() as i32);
-        score += 1 * (self.white_bishops.count_ones() as i32 - self.black_bishops.count_ones() as i32);
-        score += 1 * (self.white_rooks.count_ones() as i32 - self.black_rooks.count_ones() as i32);
-        score += 1 * (self.white_queen.count_ones() as i32 - self.black_queen.count_ones() as i32);
+        score += 3 * (self.white_knights.count_ones() as i32 - self.black_knights.count_ones() as i32);
+        score += 3 * (self.white_bishops.count_ones() as i32 - self.black_bishops.count_ones() as i32);
+        score += 5 * (self.white_rooks.count_ones() as i32 - self.black_rooks.count_ones() as i32);
+        score += 9 * (self.white_queen.count_ones() as i32 - self.black_queen.count_ones() as i32);
 
         score
     }

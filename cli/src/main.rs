7cf78091eb0955e10f8287bfdd64b33fa722fb18
Cooper Hanson <cooperhanson2020@gmail.com>
2024-07-16 15:58:28 -0500

    Got display working for cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,15 +1,7 @@
-use kno3_chess_engine::Chessboard;
 mod display;
 
-    pub trait DisplayBoard {
-        fn display(&self) -> String;
-    }
-
-    impl DisplayBoard for Chessboard {
-        fn display(&self) -> String {
-            todo!()
-        }
-    }
+use display::DisplayBoard;
+use kno3_chess_engine::Chessboard;
 
 fn main() {
     let cb = Chessboard::new();

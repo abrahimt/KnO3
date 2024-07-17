b4963b1bd85554ce392907a57ea2b442d911d7f1
Cooper Hanson <cooperhanson2020@gmail.com>
2024-07-16 20:58:30 -0500

    Added a function for getting a rooks possible moves

--- engine/src/move_generation.rs
+++ engine/src/move_generation.rs
@@ -1,6 +1,28 @@
 use super::GameState;
 
 impl GameState {
+
+    /// Move squares in iterator until a piece is hit
+    fn move_until_piece(&self, range: impl Iterator<Item = u8>, white: bool) -> Vec<u8> {
+        let mut result = Vec::new();
+
+        for square in range {
+            let piece = match self.board.piece_at_position(square) {
+                Some(p) => p,
+                None => { result.push(square); continue; }
+            };
+
+            // Am I dumb or is there no way to simplify this logic?
+            let same_color = piece.is_ascii_uppercase() && white || piece.is_ascii_lowercase() && !white;
+            if !same_color {
+                result.push(square);
+            }
+            break;
+        }
+
+        result
+    }
+
     pub fn possible_moves(&self, square: u8) -> Option<Vec<u8>> {
         let piece = match self.board.piece_at_position(square) {
             Some(p) => p,
@@ -30,7 +52,22 @@ impl GameState {
     }
 
     fn possible_rook_moves(&self, from: u8, white: bool) -> Vec<u8> {
-        todo!()
+        let mut result = Vec::new();
+        let left_bound = from - from % 8;
+        let right_bound = left_bound + 7;
+
+        result.extend(self.move_until_piece((left_bound..from).rev(), white)); // leftward moves
+        result.extend(self.move_until_piece(from+1..=right_bound, white)); // rightwards 
+        result.extend(self.move_until_piece(
+                (from + 8..=63).step_by(8),
+                white
+        ));
+        result.extend(self.move_until_piece(
+                (0..from).step_by(8).rev(),
+                white
+        ))
+
+        result
     }
 
     fn possible_bishop_moves(&self, from: u8, white: bool) -> Vec<u8> {

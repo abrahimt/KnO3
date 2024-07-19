c117b59c9c2244271dcdb0091b473e6dac442971
Cooper Hanson <cooperhanson2020@gmail.com>
2024-07-19 18:09:58 -0500

    Added comments describing what each of the modules do

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,8 +1,8 @@
-mod chessboard;
-mod state;
-mod game_state;
-mod move_generation;
+mod game_state; // FEN stuff
+mod chessboard; // board representation
+mod state; // Seeing where pieces already are
+mod move_generation; // Seeing which pieces can go where
 
-pub mod position;
+pub mod position; // converting square <-> coordinate
 pub use chessboard::Chessboard;
 pub use game_state::GameState;
